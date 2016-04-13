a27f9ffebe9542fda70e2731d607ec34c8506daf
maror6 <manish.arora83@gmail.com>
2016-04-13 23:35:39 +0530

    test message

--- PNG/Core/mongoScripts/data_commonConfig.js
+++ PNG/Core/mongoScripts/data_commonConfig.js
@@ -1,3 +1,5 @@
 db.commonConfig.insert( { propName: "PNG_IV", propValue: "F27D5C9927726BCEFE7510B1BDD3D137" } );
 db.commonConfig.insert( { propName: "PNG_PASSPHRASE", propValue: "aesalgoisbestbes" } );
 db.commonConfig.insert( { propName: "PNG_STATIC_SALT", propValue: "3288733501983511759" } );
+
+
